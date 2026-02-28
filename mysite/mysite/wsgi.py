6610fd1bbd022db9f52795cd22db229cefc1f9f3
suzdalenko-dev <alexey.saron@gmail.com>
2026-02-28 15:39:05 +0100

    su

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -5,6 +5,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+
+
+PERPLEXITI > COMET maneja su brouser desde texto
+
+CLAUDE COWORK - agente que vive en tu ordenador y puede hacer lo que hay que hacer con palabras,
+puede manejar PC, solo USA y en MAC
+
 """
 
 import os
